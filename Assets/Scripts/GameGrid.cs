7c940085d3c02034c6c3684e5a4458629f67c4aa
Evan Rittenhouse <emanguy@gmail.com>
2018-06-30 17:16:59 -0400

    WIP - Initial commit

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    public GameObject BoundObject;
+
+    private GameTile[,] _tiles;
+    
+    private void Start()
+    {
+        var retrievedTiles = BoundObject.GetComponentsInChildren<GameTile>();
+
+        _tiles = new GameTile[retrievedTiles.Max(tile => tile.Row), retrievedTiles.Max(tile => tile.Col)];
+    }
+}
